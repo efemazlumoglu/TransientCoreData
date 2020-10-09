d036747481ca8ec12ebf6de3bf960a893bbe4472
efemazlumoglu <efe.mazlumoglu@gmail.com>
2020-10-09 13:45:07 +0300

    nil point exception fixed now it is working

--- transient/AppDelegate.swift
+++ transient/AppDelegate.swift
@@ -45,7 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         lazy var managedObjectModel: NSManagedObjectModel = {
             // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
             let modelURL = Bundle.main.url(forResource: "Data", withExtension: "momd")
-            print(Bundle.main.url(forResource: "Data", withExtension: "momd"))
             return NSManagedObjectModel(contentsOf: modelURL!)!
         }()
 
